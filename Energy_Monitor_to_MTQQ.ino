a394af3fa54426c3faddcf56bbc185f3f100cc5d
luizmotta <motta473@familiamotta.com.br>
2020-01-25 14:06:19 +0000

    Added OTA and separated setup functions

--- Energy_Monitor_to_MTQQ.ino
+++ Energy_Monitor_to_MTQQ.ino
@@ -86,20 +86,62 @@ void MQTT_connect() {
 /*
  * SETUP
  */
- 
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);    // turn the LED off by making the voltage LOW
 
-  Serial.begin(115200);
-  delay(10);
+void setupOTA() {
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
 
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();  
+}
+
+void setupWifi() {
   // Connect to WiFi network
-  Serial.println();
   Serial.println();
   Serial.print( "Connecting to " );
   Serial.println( WIFI_SSID );
  
+  WiFi.mode(WIFI_STA);
   WiFi.begin( WIFI_SSID, WIFI_PASSWD );
  
   while (WiFi.status() != WL_CONNECTED) {
@@ -109,6 +151,19 @@ void setup() {
   Serial.println("");
   Serial.println("WiFi connected");
   WiFi.printDiag(Serial);
+}
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);    // turn the LED off by making the voltage LOW
+
+  Serial.begin(115200);
+  delay(10);
+
+  setupWifi();
+  
+  setupOTA();
+  
   blink_ok();
 
 }
@@ -187,5 +242,7 @@ void loop() {
   }
 
   delay(20);
+
+  ArduinoOTA.handle();
 
 }
